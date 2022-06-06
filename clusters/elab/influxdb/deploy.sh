9dcc0676fd919de1ff0119fc247c5205144c5393
Marko Mudrinić <mudrinic.mare@gmail.com>
2022-06-07 01:33:59 +0200

    Some more Kubernetes magic
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- clusters/elab/influxdb/deploy.sh
+++ clusters/elab/influxdb/deploy.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+NAMESPACE=${NAMESPACE}
+
 # Add Helm repo
-helm repo add influxdata https://helm.influxdata.com
+helm repo add influxdata https://helm.influxdata.com || true
 
 # Refresh Helm repos
 helm repo update
 
 # Install InfluxDB chart
-helm upgrade --install --values values.yaml --namespace banka-dev banka-dev-influx influxdata/influxdb2
+helm upgrade --install --values ./clusters/elab/influxdb/values.yaml --namespace "${NAMESPACE}" influx influxdata/influxdb2
